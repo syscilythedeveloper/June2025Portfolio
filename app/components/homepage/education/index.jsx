df94c7a99cbbf684446dbd50ccf2a51058aee197
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-06-25 06:33:38 -0400

    Removing Lottie

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -2,8 +2,6 @@
 import { educations } from "@/utils/data/educations";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
-import lottieFile from "../../../assets/lottie/study.json";
-import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
 
 function Education() {
@@ -38,8 +36,8 @@ function Education() {
       <div className="py-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
-            <div className="w-3/4 h-3/4">
-              <AnimationLottie animationPath={lottieFile} />
+            <div className="w-3/4 h-3/4 flex items-center justify-center">
+              <div className="text-6xl">🎓</div>
             </div>
           </div>
 
